a847ccadab2bc55503b09210874d62852a2828a6
Christian Rauch <Christian.Rauch@ed.ac.uk>
2016-09-12 12:13:35 +0100

    fix correct reconstruction of original amplitude: A = 2 * (x_spect/n)

--- fft.py
+++ fft.py
@@ -34,6 +34,7 @@ def get_spectrum(data, time):
     spectrum.append(fs)
 
     for i in range(data.shape[1]):
+        # print "mean: ", np.mean(data[:, i]) # amplitude of 0 Hz component
         # select common time base or individual time stamps
         if time.ndim == 1:
             x_interp = np.interp(dt_interp, time, data[:, i])
@@ -41,10 +42,13 @@ def get_spectrum(data, time):
             x_interp = np.interp(dt_interp, time[:,i], data[:, i])
 
         # FFT
+        # the magnitude of the spectrum is: m = A*(n/2)
+        # with 'A' the original amplitude and 'n' the length of the input signal
         x_spect = np.absolute(rfft(x_interp))
-        # convert to original units
-        x_spect = x_spect / (len(x_interp)/2.0)
-        x_spect[0] = x_spect[0] * 2
+        # convert to original amplitude units
+        # A = 2 * (x_spect/n) for all frequencies except 0Hz and Nyquist frequency, otherwise A = x_spect/n
+        x_spect /= len(x_interp)
+        x_spect[1:] *= 2.0
 
         spectrum.append(x_spect.tolist())
 
